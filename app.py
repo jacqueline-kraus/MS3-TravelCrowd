18f80b5b2f71b924f1e4b58e3f8ade637c8e6423
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-08-08 23:22:13 +0200

    added login template

--- app.py
+++ app.py
@@ -33,6 +33,11 @@ def registration():
     return render_template("registration.html")
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
