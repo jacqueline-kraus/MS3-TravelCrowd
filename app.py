c5b05c46f228cc2ba901d0aed199333518364600
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-08-08 17:21:14 +0200

    import ssl to connect to mongodb

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import ssl
 from flask import Flask, flash, render_template, redirect, request, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -9,14 +10,17 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get["MONGO_DBNAME"]
-app.config["MONGO_URI"] = os.environ.get["MONGO_URI"]
-app.secret_key = os.environ.get["SECRET_KEY"]
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
-mongo = PyMongo(app )
+mongo = PyMongo(app, ssl_cert_reqs=ssl.CERT_NONE)
 
-
-
+@app.route("/")
+@app.route("/get_deals")
+def get_deals():
+    deals = mongo.db.deals.find()
+    return render_template("deals.html", deals=deals)
 
 
 
