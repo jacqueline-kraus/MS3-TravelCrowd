fccfd759c3281ffd53a18087a7a72e51f1895c5f
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-08-11 16:14:34 +0200

    connected profile page to project

--- app.py
+++ app.py
@@ -50,12 +50,24 @@ def registration():
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
         return redirect(url_for("profile", username=session["user"]))
+
     return render_template("registration.html")
 
 
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    #grab the sessions user username from db
+    username = mongo.db.users.find_one({"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+    return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
