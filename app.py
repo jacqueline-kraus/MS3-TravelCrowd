5ad6c92faab530c247987202e9307554f243b84f
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-08-11 15:38:15 +0200

    added register funcitonality

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import ssl
 from flask import Flask, flash, render_template, redirect, request, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -27,8 +28,28 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/registration")
+@app.route("/registration", methods=["GET", "POST"])
 def registration():
+    # registration is the same as in CI videos
+    if request.method == "POST": 
+        #check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("registration"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("registration.html")
 
 
