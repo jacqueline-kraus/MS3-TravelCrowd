92b1871192e435a73a58c65cd3d95aa6cc25e9da
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-08-14 00:41:46 +0200

    Update form to create deal

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
+    $('input.autocomplete').autocomplete({
+      data: {
+        "USA": null,
+        "Germany": null,
+        "Italy": null,
+        "Greece": null,
+        "Spain": null,
+        "England": null,
+        "Russia": null,
+        "Austria": null,
+      },
+    });
+    $('.datepicker').datepicker();
   });
